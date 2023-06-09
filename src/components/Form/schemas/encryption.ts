a5a7dee40e3be8bdb43b4341f3d841f0302fd5a7
Vinicius Siqueira <vinicius.siqueira642@gmail.com>
2023-06-08 23:11:47 -0300

    fix: corrige o bug do FileList is not defined

--- src/components/Form/schemas/encryption.ts
+++ src/components/Form/schemas/encryption.ts
@@ -1,12 +1,18 @@
 import { z } from 'zod'
 
 export const encryptionFormSchema = z.object({
-  file: z
-    .instanceof(FileList)
-    .refine((fileList) => fileList.length > 0, 'O arquivo é obrigatório!')
-    .refine(
-      (fileList) => fileList?.[0]?.type === 'text/plain',
-      'O arquivo deve ser um arquivo TXT!',
-    ),
+  file: z.custom((value) => {
+    if (typeof window !== 'undefined') {
+      if (!(value instanceof FileList)) {
+        throw new Error('O arquivo é obrigatório!')
+      }
+
+      if (value.length === 0 || value[0].type !== 'text/plain') {
+        throw new Error('O arquivo deve ser um arquivo TXT!')
+      }
+    }
+
+    return true
+  }),
   publicKey: z.string().nonempty('A chave pública é obrigatória!'),
 })
